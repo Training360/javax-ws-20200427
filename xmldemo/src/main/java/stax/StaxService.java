c805b14e2855bb147be548dc53c4c03d8b726aab
vicziani <viczian.istvan@gmail.com>
2020-04-27 16:27:00 +0200

    Stax beolvasás

---
+++ xmldemo/src/main/java/stax/StaxService.java
@@ -0,0 +1,55 @@
+package stax;
+
+import dom.DomService;
+import sax.SaxService;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StaxService {
+
+    public List<String> readIsbn10Numbers(Reader source) {
+        // TODO
+        return null;
+    }
+
+    public List<String> readTitles(Reader source) {
+        try {
+            List<String> titles = new ArrayList<>();
+
+            var f = XMLInputFactory.newInstance();
+            var r = f.createXMLStreamReader(source);
+
+            while (r.hasNext()) {
+                if (r.getEventType() == XMLStreamConstants.START_ELEMENT) {
+                    if (r.getName().getLocalPart().equals("title")) {
+                        titles.add(r.getElementText());
+                    }
+                }
+                r.next();
+            }
+            return titles;
+        }
+        catch (Exception e) {
+            throw new IllegalStateException("Can not read", e);
+        }
+    }
+
+    public static void main(String[] args) {
+            var reader = new BufferedReader(new InputStreamReader(
+                    DomService.class.getResourceAsStream("/catalog.xml")
+            ));
+            try (reader) {
+                System.out.println(new StaxService().readTitles(reader));
+            }
+            catch (IOException ioe) {
+                throw new IllegalStateException("Can not read file", ioe);
+            }
+    }
+}
